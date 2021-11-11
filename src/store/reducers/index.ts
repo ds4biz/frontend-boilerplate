98a75198bfb6ec747c8c919fa3e0aaf125ed71af
Alessandro Pisu <a.pisu@ilivetech.it>
2021-11-11 10:23:36 +0100

    Add prettier dependency

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,11 +1,11 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import { connectRouter } from "connected-react-router";
-import { createBrowserHistory } from "history";
-import { examplesStore } from "./examples";
+import { combineReducers } from '@reduxjs/toolkit';
+import { connectRouter } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
+import { examplesStore } from './examples';
 
 const history = createBrowserHistory();
 
 export default combineReducers({
-	example: examplesStore.reducer,
-	router: connectRouter(history),
+  example: examplesStore.reducer,
+  router: connectRouter(history),
 });
